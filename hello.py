c7918642412c73b28f4cc77c29c750089995ca91
AmmyRongsawad <125855433+AmmyRongsawad@users.noreply.github.com>
2023-02-20 13:56:13 +0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,12 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('Hello Uncle')
+Hello Uncle
+>>> print('Hell Sochai')
+Hell Sochai
+>>> print('Hello Github')
+Hello Github
+>>> friend=['Loong','Pa lek','Loong Dam']
+>>> friend=['Loong','Pa lek','Loong Dam']
+>>> print(friend[0])
+Loong
